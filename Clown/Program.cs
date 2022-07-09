7f58423601319587e0b9dedc91da87350847667c
Brandon Sia <brandonsia@hotmail.com>
2022-07-09 23:08:20 +1000

    Add project files.

---
+++ Clown/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Clown
+{
+    interface IClown
+    {
+        string FunnyThingIHave { get; }
+        void Honk();
+    }
+    interface IScaryClown : IClown
+    {
+        string ScaryThingIHave { get; }
+        void ScareLittleChildren();
+    }
+
+    class FunnyFunny:IClown
+    {
+        private string funnyThingIHave;
+        public string FunnyThingIHave
+        {
+            get { return funnyThingIHave; }
+        }
+
+        public FunnyFunny(string thing)
+        {
+            funnyThingIHave = thing;
+        }
+        public void Honk()
+        {
+            Console.WriteLine("Hi kids! I have a " + FunnyThingIHave + ".");
+        }
+    }
+
+    class ScaryScary : FunnyFunny,IScaryClown
+    {
+        private readonly int scaryThingCount;
+        public ScaryScary(int scaryThingCount, string funnyThingIHave):base(funnyThingIHave)
+        {
+            this.scaryThingCount = scaryThingCount;
+        }
+        public string ScaryThingIHave
+        {
+            get { return scaryThingCount + " spiders"; }
+        }
+
+        public void ScareLittleChildren()
+        {
+            Console.WriteLine($"Boo! Gotcha! Look at my {ScaryThingIHave}");
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IClown fingersTheClown = new ScaryScary(14, "big red nose");
+            fingersTheClown.Honk();
+
+            if (fingersTheClown is IScaryClown iScaryClownReference)
+            {
+                iScaryClownReference.ScareLittleChildren();
+            }
+            
+        }
+    }
+}
